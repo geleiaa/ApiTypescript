9c6ed21a55221e750ae266e2a99ccb67523d2264
geleia <geleia@mail.com>
2023-03-08 03:57:36 -0300

    second commit

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
-const routes = Router()
+const routes = Router();
 
-routes.get('/', (req, res) =>{
-    res.json({message: 'Hello Dev'})
-})
+routes.get('/', (req, res) => {
+  res.json({ message: 'Hello Dev' });
+});
 
 export default routes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,30 @@
-import express from 'express'
-import cors from 'cors'
-import routes from './routes'
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
-const port = 1234
-app.listen(port, () => {
-    console.log(`Server started in port ${port}...`);
+app.use((err: Error, req: Request, res: Response, next: NextFunction ) =>{
+  if(err instanceof AppError){
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error '
+  })
 })
+
+const port = 1234;
+app.listen(port, () => {
+  console.log(`Server started in port ${port}...`);
+});
